f55c25fe9d2d92192b610acd4d91431ed3dd350f
dommad <dmadej@ust.hk>
2023-11-01 15:58:26 +0800

    add cmd line support, examples

--- main_app.py
+++ main_app.py
@@ -1,4 +1,5 @@
 """Dash app for label-free quantification of proteomics data"""
+import argparse
 from dash import Dash, dcc, html, dash_table, Input, Output, State
 import numpy as np
 import pandas as pd
@@ -7,183 +8,166 @@ import quantify as qu
 
 
 
+def execute_main(c_files, t_files):
 
-app = Dash(__name__)
+    controls_min = qu.generate_master_dict(c_files, impute_method="minimum")
+    treatments_min = qu.generate_master_dict(t_files, impute_method="minimum")
+    controls_ave = qu.generate_master_dict(c_files, impute_method="average")
+    treatments_ave = qu.generate_master_dict(t_files, impute_method="average")
 
+    app = Dash(__name__)
 
-#app = Dash(__name__)
-
-
-def get_res(files, impute_method="average"):
-    """combine results from multiple files"""
-    results = qu.Quantify().generate_master_dict(files, impute_method)
-    return results
-
-# load data from the files
-controls_files = ("interact-f07.ipro.prot.xml", "interact-f09.ipro.prot.xml")
-treatments_files = ("interact-f10.ipro.prot.xml", "interact-f42.ipro.prot.xml")
-
-controls_min = get_res(controls_files, impute_method="minimum")
-treatments_min = get_res(treatments_files,impute_method="minimum")
-controls_ave = get_res(controls_files, impute_method="average")
-treatments_ave = get_res(treatments_files, impute_method="average")
-
-app = Dash(__name__)
-
-app.layout = html.Div(
-    style={
-        'display': 'flex',
-        'margin-left': '20px'
-        },
-        children = [
-    html.Div(style={'display': 'inline-block', 'width': '50%'},
-    children=[
-    html.H1("Quantification of Proteomics Data"),
-    html.Br(),
-    html.H2("Select Input files"),
-    dcc.Input(
-        'testing',
-        id='input-control-files'
-        ),
-    html.H2("Quantification method"),
-    dcc.RadioItems(
-        ['NSAF', 'SI'],
-        'SI',
-        id='select-quant-method'
-        ),
-    html.Br(),
-    html.H2("Select FDR threshold"),
-    dcc.Slider(
-        0, 0.05, step=None,
-        id='select-fdr',
-        marks=dict(
-            zip(
-                np.linspace(0, 0.05, 11),
-                [str(i) for i in np.linspace(0, 0.1, 11)]
-                )
-                ),
-        value=0.01
-    ),
-    html.Br(),
-    html.H2("Select log2(fold change) (absolute)"),
-    dcc.Slider(id='select-fold-change',
-            min=0,
-            max=5,
-            marks={i: str(i) for i in np.linspace(0, 5, 11)},
-            value=1.5,
-        ),
-    html.Br(),
-    html.H2("Select imputation method for missing values"),
-    dcc.Dropdown(
-        ['Average', 'Minimum in replicate'],
-        'Minimum in replicate',
-        id='select-imputation'
-        )]),
-
-    html.Div(
+    app.layout = html.Div(
         style={
-            'display': 'inline-block',
-            'width': '50%',
-            'margin-left': '20px',
-            'margin-right': '20px',
-            'margin-top': '20px'
+            'display': 'flex',
+            'margin-left': '20px'
             },
+            children = [
+        html.Div(style={'display': 'inline-block', 'width': '50%'},
         children=[
-            html.Div(
-                id='volcano-plot'
-                    ),
-            html.Div(
-                style={
-                    'margin-left': '20px',
-                    'margin-right': '20px',
-                    'margin-top': '20px'},
-                id='data-table'
-                    ),
-
-            html.Div(id='button-div', children=[html.Button('Export Table', className='button-79',
-                                 id='export-button', n_clicks=0)],
-        style={
-            'margin-left': '20px',
-            'margin-right': '20px',
-            'margin-top': '20px'},
+        html.H1("Quantification of Proteomics Data"),
+        html.Br(),
+        html.H2("Quantification method"),
+        dcc.RadioItems(
+            ['NSAF', 'SI'],
+            'SI',
+            id='select-quant-method'
             ),
-])]
-)
+        html.Br(),
+        html.H2("Select FDR threshold"),
+        dcc.Slider(
+            0, 0.05, step=None,
+            id='select-fdr',
+            marks={int(i) if i % 1 == 0 else i: '{}'.format(i) for i in np.linspace(0, 0.05, 11)},
+            value=0.01
+        ),
+        html.Br(),
+        html.H2("Select log2(fold change) (absolute)"),
+                dcc.Slider(
+                           0.0, 5.0, step=None, id='select-fold-change',
+                marks={int(i) if i % 1 == 0 else i: '{}'.format(i) for i in np.linspace(0, 5, 11)},
+                value=1.5
+            ),
+        html.Br(),
+        html.H2("Select imputation method for missing values"),
+        dcc.Dropdown(
+            ['Average', 'Minimum in replicate'],
+            'Minimum in replicate',
+            id='select-imputation'
+            )]),
+
+        html.Div(
+            style={
+                'display': 'inline-block',
+                'width': '50%',
+                'margin-left': '20px',
+                'margin-right': '20px',
+                'margin-top': '20px'
+                },
+            children=[
+                html.Div(
+                    id='volcano-plot'
+                        ),
+                html.Div(
+                    style={
+                        'margin-left': '20px',
+                        'margin-right': '20px',
+                        'margin-top': '20px'},
+                    id='data-table'
+                        ),
+
+    ])]
+    )
 
 
-@app.callback(
-[Output('volcano-plot', 'children'),
-Output('data-table', 'children')],
-Input('select-fdr', 'value'),
-Input('select-fold-change', 'value'),
-Input('select-quant-method', 'value'),
-Input('select-imputation', 'value'))
-def update_volcano(fdr_lim, fold_lim, q_mode, imput):
-    """update the volcano plot"""
-    qmodes = {'SI': 'sin', 'NSAF': 'nsaf'}
+    @app.callback(
+    [Output('volcano-plot', 'children'),
+    Output('data-table', 'children')],
+    Input('select-fdr', 'value'),
+    Input('select-fold-change', 'value'),
+    Input('select-quant-method', 'value'),
+    Input('select-imputation', 'value'))
+    def update_volcano(fdr_lim, fold_lim, q_mode, imput):
+        """update the volcano plot"""
+        qmodes = {'SI': 'sin', 'NSAF': 'nsaf'}
 
-    if imput == "Average":
-        new_d = qu.Quantify().welch_test(controls_ave, treatments_ave, qmodes[q_mode])
-    else:
-        new_d = qu.Quantify().welch_test(controls_min, treatments_min, qmodes[q_mode])
+        if imput == "Average":
+            new_d = qu.welch_test(controls_ave, treatments_ave, qmodes[q_mode])
+        else:
+            new_d = qu.welch_test(controls_min, treatments_min, qmodes[q_mode])
 
-    sel_data = qu.Quantify().get_stats(new_d, fdr_lim, fold_lim)
+        sel_data = qu.get_stats(new_d, fdr_lim, fold_lim)
 
-    new_fig = go.Figure(data=[
-                        go.Scattergl(
-                        y=[-np.log(el[1][0]) for el in sel_data],
-                        x=[el[1][1] for el in sel_data],
-                        mode='markers',
-                        text=[x[0] for x in sel_data],
-                        )
-                        ],
-                        layout={
-                            "xaxis": {'title': "log2(fold change)"},
-                            "yaxis": {'title': "-log(p-value)"}
-                            }
-                        )
+        new_fig = go.Figure(data=[
+                            go.Scattergl(
+                            y=[-np.log(el[1][0]) for el in sel_data],
+                            x=[el[1][1] for el in sel_data],
+                            mode='markers',
+                            text=[x[0] for x in sel_data],
+                            )
+                            ],
+                            layout={
+                                "xaxis": {'title': "log2(fold change)"},
+                                "yaxis": {'title': "-log(p-value)"}
+                                }
+                            )
 
-    dash_t = get_table(sel_data)
-    return dcc.Graph(figure=new_fig), dash_t
+        dash_t = get_table(sel_data)
+        return dcc.Graph(figure=new_fig), dash_t
 
 
-def get_table(sel_data):
-    """generate dash table from the quantification data provided"""
-    tab_df = pd.DataFrame.from_dict(
-        dict(sel_data),
-        orient='index',
-        columns=['p-value', 'log2(fold change)']
-        )
-    tab_df['protein'] = tab_df.index
-    tab_df.reset_index(inplace=True, drop=True)
+    def get_table(sel_data):
+        """generate dash table from the quantification data provided"""
+        tab_df = pd.DataFrame.from_dict(
+            dict(sel_data),
+            orient='index',
+            columns=['p-value', 'log2(fold change)']
+            )
+        tab_df['protein'] = tab_df.index
+        tab_df.reset_index(inplace=True, drop=True)
 
-    dash_t = dash_table.DataTable(
-        id='table-proteins',
-        columns=[
-                {"name": i, "id": i, "deletable": True, "selectable": True} for i in tab_df.columns
-                ],
-        data=tab_df.to_dict('records'),
-        editable=True,
-        filter_action="native",
-        sort_action="native",
-        sort_mode="multi",
-        column_selectable="single",
-        row_selectable="multi",
-        row_deletable=True,
-        selected_columns=[],
-        selected_rows=[],
-        page_action="native",
-        page_current= 0,
-        page_size= 10,
-        style_cell={
-                    'backgroundColor':'rgba(223, 234, 185, 0.23)',
-                    'opacity':0.95,
-                    'color': 'black'
-                    }
-        )
-    return dash_t
+        dash_t = dash_table.DataTable(
+            id='table-proteins',
+            columns=[
+                    {"name": i, "id": i, "deletable": True, "selectable": True} for i in tab_df.columns
+                    ],
+            data=tab_df.to_dict('records'),
+            editable=True,
+            filter_action="native",
+            sort_action="native",
+            sort_mode="multi",
+            column_selectable="single",
+            row_selectable="multi",
+            row_deletable=True,
+            selected_columns=[],
+            selected_rows=[],
+            page_action="native",
+            page_current= 0,
+            page_size= 10,
+            style_cell={
+                        'backgroundColor':'rgba(223, 234, 185, 0.23)',
+                        'opacity':0.95,
+                        'color': 'black'
+                        },
+            export_format='xlsx',
+            export_headers='display',
+            )
+        return dash_t
+    
+    app.run_server(debug=True)
 
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+
+    parser = argparse.ArgumentParser(description='Quantification of shotgun proteomics data')
+    parser.add_argument('-c',  '--control', nargs='*', required=True,
+                        type=str, help='prot.xml file(s) output by StPeter containing data for control replicates')
+    parser.add_argument('-t', '--treatment', nargs='*', required=True,
+                        type=str, help="prot.xml file(s) output by StPeter containing data for treatment replicates")
+    parser.add_argument('-o', '--output')
+    args = parser.parse_args()
+    c_files = args.control
+    t_files = args.treatment
+    output_name = args.output
+    execute_main(c_files, t_files)
--- quantify.py
+++ quantify.py
@@ -5,170 +5,156 @@ from pyteomics import protxml
 import numpy as np
 import scipy.stats as st
 import matplotlib.pyplot as plt
-from functools import lru_cache
 
 
-class Quantify:
-    """class for quantification of proteomics data using
-    SIn or NSAF"""
+def generate_master_dict(lists, impute_method='average'):
+    """Generate master dictionary"""
+    master_dict = {}
+    rep_no = len(lists)
 
-    def __init__(self):
-        pass
+    for idx, file_name in enumerate(lists):
+        cur_list = get_protein_list(file_name)
+        for prot, cur_sin, cur_nsaf in cur_list:
+            if prot not in master_dict:
+                master_dict[prot] = {'sin': rep_no*[None,], 'nsaf': rep_no*[None,]}
+            master_dict[prot]['sin'][idx] = float(cur_sin)
+            master_dict[prot]['nsaf'][idx]  = float(cur_nsaf)
 
+    master_dict = impute_missing_values(master_dict, rep_no, impute_method)
 
-    def generate_master_dict(self, lists, impute_method='average'):
-        """Generate master dictionary"""
-        master_dict = {}
-        rep_no = len(lists)
-
-        for idx, file_name in enumerate(lists):
-            cur_list = self.get_protein_list(file_name)
-            for prot, cur_sin, cur_nsaf in cur_list:
-                if prot not in master_dict:
-                    master_dict[prot] = {'sin': rep_no*[None,], 'nsaf': rep_no*[None,]}
-                master_dict[prot]['sin'][idx] = float(cur_sin)
-                master_dict[prot]['nsaf'][idx]  = float(cur_nsaf)
-
-        master_dict = self.impute_missing_values(master_dict, rep_no, impute_method)
-
-        return master_dict
+    return master_dict
 
 
-    def get_protein_list(self, file_name):
-        """Get the protein list"""
+def get_protein_list(file_name):
+    """Get the protein list"""
 
-        data = protxml.read(file_name)
-        protein_info = [self.get_quant_data(x) for x in data]
-        protein_info = [x for x in protein_info if x != ()]
+    data = protxml.read(file_name)
+    protein_info = [get_quant_data(x) for x in data]
+    protein_info = [x for x in protein_info if x != ()]
 
-        return protein_info
-
-
-    @staticmethod
-    def get_quant_data(item):
-        """extract protein data from an entry in protXML file"""
-        protein = item['protein'][0]
-        if 'analysis_result' in protein.keys():
-            protein_name = protein['protein_name'].split('|')[-1]
-            quant_data = protein['analysis_result'][0]['StPeterQuant']
-            si_n = np.float32(quant_data['SIn'])
-            nsaf = np.float32(quant_data['NSAF'])
-            return (protein_name, si_n, nsaf)
-        return ()
+    return protein_info
 
 
-    @staticmethod
-    def impute_missing_values(all_dicts, no_rep, method='average'):
-        """Impute the missing values"""
-        def impute_ave(scores):
-            sin = scores['sin']
-            ave_sin = np.mean([x for x in sin if x is not None])
-            nsaf = scores['nsaf']
-            ave_nsaf = np.mean([x for x in nsaf if x is not None])
-
-            new_sin = [x if isinstance(x, float) else ave_sin + np.random.rand()*0.001 for x in sin ]
-            new_nsaf = [x if isinstance(x, float) else ave_nsaf + np.random.rand()*0.001 for x in nsaf]
-
-            return {'sin': new_sin, 'nsaf': new_nsaf}
-
-        def get_minima(all_dicts, no_rep):
-            min_sins = []
-            min_nsafs = []
-            for idx in range(no_rep):
-                min_sin = min((all_dicts[x]['sin'][idx] for x in all_dicts.keys() if all_dicts[x]['sin'][idx] is not None))
-                min_nsaf = min((all_dicts[x]['nsaf'][idx] for x in all_dicts.keys() if all_dicts[x]['sin'][idx] is not None))
-                min_sins.append(min_sin)
-                min_nsafs.append(min_nsaf)
-            return min_sins, min_nsafs
-        
-        def impute_min(scores, min_sins, min_nsafs):
-
-            new_sin = [x if isinstance(x, float) else min_sins[idx] + np.random.rand()*0.001 for idx, x in enumerate(scores['sin']) ]
-            new_nsaf = [x if isinstance(x, float) else min_nsafs[idx] + np.random.rand()*0.001 for idx, x in enumerate(scores['nsaf'])]
-
-            return {'sin': new_sin, 'nsaf': new_nsaf}
+def get_quant_data(item):
+    """extract protein data from an entry in protXML file"""
+    protein = item['protein'][0]
+    if 'analysis_result' in protein.keys():
+        protein_name = protein['protein_name'].split('|')[-1]
+        quant_data = protein['analysis_result'][0]['StPeterQuant']
+        si_n = np.float32(quant_data['SIn'])
+        nsaf = np.float32(quant_data['NSAF'])
+        return (protein_name, si_n, nsaf)
+    return ()
 
 
-        if method == 'average':
-            new_dict = dict((k, impute_ave(v)) if ((None in v['sin']) | (None in v['nsaf'])) else (k, v) for k, v in all_dicts.items())
+def impute_missing_values(all_dicts, no_rep, method='average'):
+    """Impute the missing values"""
+    def impute_ave(scores):
+        sin = scores['sin']
+        ave_sin = np.mean([x for x in sin if x is not None])
+        nsaf = scores['nsaf']
+        ave_nsaf = np.mean([x for x in nsaf if x is not None])
 
-        if method == 'minimum':
-            min_sins, min_nsafs = get_minima(all_dicts, no_rep)
-            new_dict = dict((k, impute_min(v, min_sins, min_nsafs)) if ((None in v['sin']) | (None in v['nsaf'])) else (k, v) for k, v in all_dicts.items())
+        new_sin = [x if isinstance(x, float) else ave_sin + np.random.rand()*0.001 for x in sin ]
+        new_nsaf = [x if isinstance(x, float) else ave_nsaf + np.random.rand()*0.001 for x in nsaf]
 
-        return new_dict
+        return {'sin': new_sin, 'nsaf': new_nsaf}
 
+    def get_minima(all_dicts, no_rep):
+        min_sins = []
+        min_nsafs = []
+        for idx in range(no_rep):
+            min_sin = min((all_dicts[x]['sin'][idx] for x in all_dicts.keys() if all_dicts[x]['sin'][idx] is not None))
+            min_nsaf = min((all_dicts[x]['nsaf'][idx] for x in all_dicts.keys() if all_dicts[x]['sin'][idx] is not None))
+            min_sins.append(min_sin)
+            min_nsafs.append(min_nsaf)
+        return min_sins, min_nsafs
+    
+    def impute_min(scores, min_sins, min_nsafs):
 
-    @staticmethod
-    def welch_test(conts, treats, mode='sin'):
-        """get p-values for Welch t-test between control and treatment"""
-        # extract list of all proteins shared by the control and treatments
-        all_proteins = set.union(set(conts.keys()), set(treats.keys()))
+        new_sin = [x if isinstance(x, float) else min_sins[idx] + np.random.rand()*0.001 for idx, x in enumerate(scores['sin']) ]
+        new_nsaf = [x if isinstance(x, float) else min_nsafs[idx] + np.random.rand()*0.001 for idx, x in enumerate(scores['nsaf'])]
 
-        output = {} # protein: (p_value, log2(fold change)
-        for prot in all_proteins:
-            if prot not in conts:
-                output[prot] = (1e-16, 5) # protein definitely upregulated
-            elif prot not in treats:
-                output[prot] = (1e-16, -5) # protein definitely downregulated
-            else:
-                cont_vals = conts[prot][mode]
-                treat_vals = treats[prot][mode]
-                p_val = st.ttest_ind(cont_vals, treat_vals, equal_var=False).pvalue
-                output[prot] = (p_val, np.mean(treat_vals)-np.mean(cont_vals))
-
-        return output
-
-
-    @staticmethod
-    def volcano_plot(data, fdr_lim, fold_lim):
-        """generate volcano plot for the quantification data with
-        return information about down- and up-regulated proteins"""
-        #pval_cutoff = -np.log(self.get_fdr_threshold(data, fdr_lim))
-
-        fold_vals = [x[1] for x in data.values()]
-        log_pvals = -np.log10([x[0] for x in data.values()])
-
-        c_accept = 'green'
-        c_reject = (0.90196078, 0.81568627, 0.85882353, 0.8)
-        cutoff_style = 'dotted'
-        cutoff_color = 'grey'
-
-        x_lims = (min(fold_vals)-1, max(fold_vals)+1)
-        y_lims = (0, max(log_pvals) + 1)
+        return {'sin': new_sin, 'nsaf': new_nsaf}
 
 
-        colors = [c_accept if abs(x) > fold_lim and abs(y) > fdr_lim else c_reject for (x, y) in zip(fold_vals, log_pvals)]
-        
-        fig, axes = plt.subplots(figsize=(5,5))
-        axes.scatter(fold_vals, log_pvals, s=5, color=colors)
-        axes.hlines(y=fdr_lim, xmin=x_lims[0], xmax=x_lims[1], linestyles=cutoff_style, color=cutoff_color)
-        axes.vlines(x=[fold_lim, -fold_lim], ymin=y_lims[0], ymax=y_lims[1], linestyles=cutoff_style, color=cutoff_color)
-        axes.set_xlabel("log2(fold change)")
-        axes.set_ylabel("-log(p-value)")
-        axes.set_ylim(y_lims[0], y_lims[1])
-        axes.set_xlim(x_lims[0], x_lims[1])
-        fig.tight_layout()
+    if method == 'average':
+        new_dict = dict((k, impute_ave(v)) if ((None in v['sin']) | (None in v['nsaf'])) else (k, v) for k, v in all_dicts.items())
+
+    elif method == 'minimum':
+        min_sins, min_nsafs = get_minima(all_dicts, no_rep)
+        new_dict = dict((k, impute_min(v, min_sins, min_nsafs)) if ((None in v['sin']) | (None in v['nsaf'])) else (k, v) for k, v in all_dicts.items())
+
+    return new_dict
+
+
+def welch_test(conts, treats, mode='sin'):
+    """get p-values for Welch t-test between control and treatment"""
+    # extract list of all proteins shared by the control and treatments
+    all_proteins = set.union(set(conts.keys()), set(treats.keys()))
+
+    output = {} # protein: (p_value, log2(fold change)
+    for prot in all_proteins:
+        if prot not in conts:
+            output[prot] = (1e-16, 5) # protein definitely upregulated
+        elif prot not in treats:
+            output[prot] = (1e-16, -5) # protein definitely downregulated
+        else:
+            cont_vals = conts[prot][mode]
+            treat_vals = treats[prot][mode]
+            p_val = st.ttest_ind(cont_vals, treat_vals, equal_var=False).pvalue
+            output[prot] = (p_val, np.mean(treat_vals)-np.mean(cont_vals))
+
+    return output
 
 
 
-    def get_stats(self, data, fdr_lim, fold_lim):
-        """get the p-value cutoff for the given FDR threshold
-        and output the list of up and downregulated proteins"""
-        pval_th = self.get_fdr_threshold(data, fdr_lim)
-        sel_data = [x for x in data.items() if (x[1][0] <= pval_th and  abs(x[1][1]) >= fold_lim)]
-        #up_reg = [x for x in data.items() if (x[1][0] <=pval_cutoff and  x[1][1] >= fold_lim)]
-        #down_reg = [x for x in data.items() if (x[1][0] <=pval_cutoff and  -x[1][1] >= fold_lim)]
-        return sel_data
+def volcano_plot(data, fdr_lim, fold_lim):
+    """generate volcano plot for the quantification data with
+    return information about down- and up-regulated proteins"""
+    #pval_cutoff = -np.log(self.get_fdr_threshold(data, fdr_lim))
+
+    fold_vals = [x[1] for x in data.values()]
+    log_pvals = -np.log10([x[0] for x in data.values()])
+
+    c_accept = 'green'
+    c_reject = (0.90196078, 0.81568627, 0.85882353, 0.8)
+    cutoff_style = 'dotted'
+    cutoff_color = 'grey'
+
+    x_lims = (min(fold_vals)-1, max(fold_vals)+1)
+    y_lims = (0, max(log_pvals)+1)
 
 
-    @staticmethod
-    def get_fdr_threshold(data, fdr_lim):
-        """find the adjusted p-value cutoff for the specified
-        FDR threshold"""
-        q_idx = 1 # index of quantification data in the tuple
-        p_idx = 0 # index of p-value in the quantification data tuple
-        sorted_d = sorted(data.items(), key=lambda x: x[q_idx][p_idx]) # sort by p-values
-        adj_ps = [(idx+1)*val[1][0]/len(sorted_d) for idx, val in enumerate(sorted_d)]
-        cutoff = sorted_d[len([x for x in adj_ps if x <= fdr_lim])-1][q_idx][p_idx]
-        return cutoff
+    colors = [c_accept if abs(x) > fold_lim and abs(y) > fdr_lim else c_reject for (x, y) in zip(fold_vals, log_pvals)]
+    
+    fig, axes = plt.subplots(figsize=(5,5))
+    axes.scatter(fold_vals, log_pvals, s=5, color=colors)
+    axes.hlines(y=fdr_lim, xmin=x_lims[0], xmax=x_lims[1], linestyles=cutoff_style, color=cutoff_color)
+    axes.vlines(x=[fold_lim, -fold_lim], ymin=y_lims[0], ymax=y_lims[1], linestyles=cutoff_style, color=cutoff_color)
+    axes.set_xlabel("log2(fold change)")
+    axes.set_ylabel("-log(p-value)")
+    axes.set_ylim(y_lims[0], y_lims[1])
+    axes.set_xlim(x_lims[0], x_lims[1])
+    fig.tight_layout()
+
+
+
+def get_stats(data, fdr_lim, fold_lim):
+    """get the p-value cutoff for the given FDR threshold
+    and output the list of up and downregulated proteins"""
+    pval_th = get_fdr_threshold(data, fdr_lim)
+    sel_data = [x for x in data.items() if (x[1][0] <= pval_th and  abs(x[1][1]) >= fold_lim)]
+    return sel_data
+
+
+
+def get_fdr_threshold(data, fdr_lim):
+    """find the adjusted p-value cutoff for the specified
+    FDR threshold"""
+    q_idx = 1 # index of quantification data in the tuple
+    p_idx = 0 # index of p-value in the quantification data tuple
+    sorted_d = sorted(data.items(), key=lambda x: x[q_idx][p_idx]) # sort by p-values
+    adj_ps = [(idx + 1) * val[1][0] / len(sorted_d) for idx, val in enumerate(sorted_d)]
+    cutoff = sorted_d[len([x for x in adj_ps if x <= fdr_lim])-1][q_idx][p_idx]
+    return cutoff
